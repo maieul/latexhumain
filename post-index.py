e01485932dafa67452064671050b705323d2c896
maieul <maieul@maieul.net>
2011-10-10 13:46:09 +0000

    un try dans le script python pour ne pas pas planter tout le makefile

--- post-index.py
+++ post-index.py
@@ -3,23 +3,26 @@
 
 # Ouverture du fichier
 import codecs,re
-lecture = codecs.open('principal-cs.ind',encoding='utf-8')
-sortie =[]
+try:
+    lecture = codecs.open('principal-cs.ind',encoding='utf-8')
+    sortie =[]
 
 
 #fusion des pages en gras redondante avec pages en maigres
-regexp = "(\\\\hyperpage{(?P<id>[0-9]+)}, \\\\hyperpagetextbf{(?P=id)})"
-for ligne in lecture:
+    regexp = "(\\\\hyperpage{(?P<id>[0-9]+)}, \\\\hyperpagetextbf{(?P=id)})" 
+    for ligne in lecture:
    
-    resultat = re.findall(regexp,ligne)
-    if len(resultat) == 1:
-        sortie.append(ligne.replace('\hyperpage{'+resultat[0][1]+'}, ',''))
-    else:
-        sortie.append(ligne)
+        resultat = re.findall(regexp,ligne)
+        if len(resultat) == 1:
+            sortie.append(ligne.replace('\hyperpage{'+resultat[0][1]+'}, ',''))
+        else:
+            sortie.append(ligne)
 
 
 
 # Ecriture du nouveau fichier
-lecture.close()
-ecriture = codecs.open('principal-cs.ind',encoding='utf-8',mode='w')
-ecriture.writelines(sortie)
+    lecture.close()
+    ecriture = codecs.open('principal-cs.ind',encoding='utf-8',mode='w')
+    ecriture.writelines(sortie)
+except:
+    print ('Erreur dans le script python')
